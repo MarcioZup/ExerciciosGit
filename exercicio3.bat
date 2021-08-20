1f68279bc8f503117ae5b1d5b823182020b81294
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-08-20 16:51:30 -0300

    Adicionados Paises da America do Sul

--- exercicio3.bat
+++ exercicio3.bat
@@ -1,2 +1,16 @@
 md "Todos os Paises"
 cd "Todos os Paises"
+md Brasil
+md "Guiana Francesa"
+md "Suriname"
+md Guiana
+md Venezuela
+md Colombia
+md Equador
+md Peru
+md Bolivia
+md Paraguai
+md Chile
+md Argentina
+md Uruguai
+cd ..
