a598b8ccdd30b69de4c5fc123ebaf781d3fd4641
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-08-20 16:22:25 -0300

    Adicionados Estados do Nordeste

--- exercicio2.bat
+++ exercicio2.bat
@@ -6,3 +6,15 @@ md "Mato Grosso"
 md "Mato Grosso do Sul"
 md "Goias"
 md "Distrito Federal"
+cd ..
+cd "Nordeste"
+md "Maranhao"
+md "Piaui"
+md "Ceara"
+md "Rio Grande do Norte"
+md Paraiba
+md Pernambuco
+md Alagoas
+md Sergipe
+md Bahia
+cd ..
