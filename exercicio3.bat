f1238c519cecca0651d1e7a307270fadbc1635e5
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-08-20 16:56:51 -0300

    Adiconados Estados dentro da pasta Brasil

--- exercicio3.bat
+++ exercicio3.bat
@@ -14,3 +14,5 @@ md Chile
 md Argentina
 md Uruguai
 cd ..
+cd Brasil
+md "Acre" "Alagoas" "Amapa" "Amazonas" "Bahia" "Ceara" "Espirito Santo" "Goias" "Maranhao" "Mato Grosso" "Mato Grosso do Sul" "Minas Gerais" "Para" "Paraiba" "Parana" "Pernambuco" "Piaui" "Rio de Janeiro" "Rio Grande do Norte" "Rio Grande do Sul" "Rondonia" "Roraima" "Santa Catarina" "Sao Paulo" "Sergipe" "Tocantins" "Distrito Federal"
