2fd5b2df6e729b27b2ce343eaa4e63672ea60c0d
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-08-20 16:27:12 -0300

    Adicionados Estados do Sudeste

--- exercicio2.bat
+++ exercicio2.bat
@@ -18,3 +18,25 @@ md Alagoas
 md Sergipe
 md Bahia
 cd ..
+cd Norte
+md Tocantins
+md Para
+md Amapa
+md Amazonas
+md Roraima
+md Acre
+md Rondonia
+cd ..
+cd "Sul"
+md Parana
+md "Santa Catarina"
+md "Rio Grande do Sul"
+cd ..
+cd "Sudeste"
+md "Sao Paulo"
+md "Rio de Janeiro"
+md "Espirito Santo"
+md "Minas Gerais"
+
+
+
