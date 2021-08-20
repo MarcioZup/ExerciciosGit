dac40d3d779626fc16bf21b532988556c4433015
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-08-20 15:36:49 -0300

    Adicionando Clubes da Serie A

--- exercicio1.bat
+++ exercicio1.bat
@@ -2,3 +2,26 @@ md "Todos os clubes"
 cd "Todos os clubes"
 md "Serie A"
 md "Serie B"
+cd "Serie A"
+md Corinthians
+md "Atletico Mineiro"
+md Palmeiras
+md Fortaleza
+md Bragantino
+md Flamengo
+md "Atletico Paranaense"
+md "Atletico Goianiense"
+md Ceara
+md Internacional
+md santos
+md Corinthians
+md Juventude
+md Bahia
+md "Sao Paulo"
+md Fluminense
+md Cuiaba
+md Sport
+md "America - MG"
+md Gremio
+md Chapecoense
+
